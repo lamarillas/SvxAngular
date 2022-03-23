7ceb83603a9bb662a0311e2ebf113b57fc78997d
leo amarillas <43256900+lamarillas@users.noreply.github.com>
2022-03-23 15:49:14 -0700

    Home
    Home & Not Found Page

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+//import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { PresupuestoModule } from '../app/presupuesto/presupuesto.module';
+//import { PresupuestoModule } from '../app/presupuesto/presupuesto.module';
 import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.modules';
+//import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 
 @NgModule({
   declarations: [
@@ -20,21 +22,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    //PageNotFoundComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    //CoreModule,
-    //AppRoutingModule,
-    PresupuestoModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'presupuesto', loadChildren: () => import('../app/presupuesto/presupuesto.module').then(m => m.PresupuestoModule) }
-    ]),
+    CoreModule,
+    AppRoutingModule,
+    //PresupuestoModule,
+    //RouterModule.forRoot([
+    //  { path: '', component: HomeComponent, pathMatch: 'full' },
+    //  { path: 'counter', component: CounterComponent },
+    //  { path: 'fetch-data', component: FetchDataComponent },
+    //  { path: 'presupuesto', loadChildren: () => import('../app/presupuesto/presupuesto.module').then(m => m.PresupuestoModule) }
+    //]),
     BrowserAnimationsModule
   ],
   providers: [],
